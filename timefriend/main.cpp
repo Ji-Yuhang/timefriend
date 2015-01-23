e6709f6dec9c3d6b7e421ceb1aa3505e39426d89
Ji-Yuhang <yuhang.silence@gmail.com>
2015-01-23 16:01:25 +0800

    conflict

--- timefriend/main.cpp
+++ timefriend/main.cpp
@@ -6,19 +6,7 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-<<<<<<< HEAD
-    QString path = qApp->applicationDirPath();
-#ifdef Q_OS_MAC
-    path += "/../../..";
-#endif
-//    qApp->addLibraryPath(path);
-//    qApp->addLibraryPath("/Users/jiyuhang/Qt5.4.0/5.4/clang_64/plugins");
-//    qApp->addLibraryPath("/Users/jiyuhang/Qt5.4.0/5.4/clang_64/plugins/sqldrivers");
-=======
     QApplication::setQuitOnLastWindowClosed(false);
-//    QString path = qApp->applicationDirPath() + "/../../..
-//    qDebug() << "path" <<path;
->>>>>>> FETCH_HEAD
 
     MainWindow w;
     w.show();
